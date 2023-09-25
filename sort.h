2b38223f3f85d5671d3cc46260b5792fcc12ad79
Kiruti01 <mikekirutic@gmail.com>
2023-09-25 09:37:55 +0300

    task 1

--- sort.h
+++ sort.h
@@ -49,4 +49,6 @@ void print_array(const int *array, size_t size);
  */
 void print_list(const listint_t *list);
 
+/*sorting algos*/
+void insertion_sort_list(listint_t **list);
 #endif /* SORT_H */
